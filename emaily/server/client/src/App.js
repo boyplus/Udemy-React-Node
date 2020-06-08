13a37c7e6d85cd6de144916afc0689170767c41e
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-08 20:51:10 +0700

    [UPDATE] client

--- emaily/server/client/src/App.js
+++ emaily/server/client/src/App.js
@@ -16,8 +16,9 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+            Learn React
                 </a>
+          <a href="/auth/google">Sign in with google</a>
             </header>
         </div>
     );
