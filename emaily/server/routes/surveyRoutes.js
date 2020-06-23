0b7a1ec26944f6f052129f45b7b1966b0d5df2c3
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-24 03:45:40 +0700

    [ADD] email endpoint

--- emaily/server/routes/surveyRoutes.js
+++ emaily/server/routes/surveyRoutes.js
@@ -7,10 +7,13 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const { mail } = require('sendgrid');
 
 module.exports = (app) => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-        console.log('hello');
+
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thnaks for voting!');
+    });
+    
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, body, subject, recipients } = req.body;
-        console.log(title,body,subject,recipients);
         const survey = new Survey({
             title,
             body,
@@ -24,7 +27,14 @@ module.exports = (app) => {
 
         //send an email
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
-        
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
