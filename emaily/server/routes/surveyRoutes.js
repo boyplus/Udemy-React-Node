8ddbbb30f142e10b2f3e72e30d215b877acd1f64
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-26 01:34:00 +0700

    [UPDATE] send email

--- emaily/server/routes/surveyRoutes.js
+++ emaily/server/routes/surveyRoutes.js
@@ -41,7 +41,7 @@ module.exports = (app) => {
 
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
-        const events = _.chain(req.body)
+        _.chain(req.body)
             .map(({ email, url }) => {
                 const match = p.test(new URL(url).pathname);
                 if (match) {
@@ -54,8 +54,22 @@ module.exports = (app) => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: { email: email, response: false },
+                        },
+                    },
+                    {
+                        $inc: { [choice]: 1 },
+                        $set: { 'recipients.$.response': true },
+                    }
+                ).exec();
+            })
             .value();
-        console.log(events);
-        res.send({ events });
+        console.log('complete');
+        res.send({});
     });
 };
